8c24ec0732f39dd199bb290c91e39dc19218f551
Mihail Velikov <mihailvelik@gmail.com>
2015-05-03 22:05:52 +0300

    Create User class

---
+++ YouLocal/Models/User.h
@@ -0,0 +1,15 @@
+//
+//  User.h
+//  YouLocal
+//
+//  Created by Mihail Velikov on 5/3/15.
+//  Copyright (c) 2015 YouLoc.al. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface User : NSObject
+
+@property (nonatomic) NSString *userId, *gender, *fullName, *avatar50, *avatar;
+
+@end
---
+++ YouLocal/Models/User.m
@@ -0,0 +1,13 @@
+//
+//  User.m
+//  YouLocal
+//
+//  Created by Mihail Velikov on 5/3/15.
+//  Copyright (c) 2015 YouLoc.al. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+@end

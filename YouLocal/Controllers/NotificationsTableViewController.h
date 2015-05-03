7ace717ac40900808ebd0a5f72c9cb878947a78b
Mihail Velikov <mihailvelik@gmail.com>
2015-05-03 22:27:07 +0300

    Create NotificatinsTableViewController class

---
+++ YouLocal/Controllers/NotificationsTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  NotificationsTableViewController.h
+//  YouLocal
+//
+//  Created by Mihail Velikov on 5/3/15.
+//  Copyright (c) 2015 YouLoc.al. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NotificationsTableViewController : UITableViewController
+
+@end

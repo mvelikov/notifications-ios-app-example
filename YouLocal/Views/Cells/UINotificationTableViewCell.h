520bbb99dee0b323abbfbcea64719f6383e3c0e1
Mihail Velikov <mihailvelik@gmail.com>
2015-05-03 22:23:31 +0300

    Create NotificationTableViewCell class

---
+++ YouLocal/Views/Cells/UINotificationTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  UINotificationTableViewCell.h
+//  YouLocal
+//
+//  Created by Mihail Velikov on 5/3/15.
+//  Copyright (c) 2015 YouLoc.al. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UINotificationTableViewCell : UITableViewCell
+
+@end
---
+++ YouLocal/Views/Cells/UINotificationTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  UINotificationTableViewCell.m
+//  YouLocal
+//
+//  Created by Mihail Velikov on 5/3/15.
+//  Copyright (c) 2015 YouLoc.al. All rights reserved.
+//
+
+#import "UINotificationTableViewCell.h"
+
+@implementation UINotificationTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
